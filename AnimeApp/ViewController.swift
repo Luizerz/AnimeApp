bbb67e6b061dd1b34635918ef996a17748a466c9
Luiz Sena <senaluiz1307@gmail.com>
2022-10-03 11:35:09 -0300

    setup first commit

---
+++ AnimeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeApp
+//
+//  Created by Luiz Sena on 03/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
